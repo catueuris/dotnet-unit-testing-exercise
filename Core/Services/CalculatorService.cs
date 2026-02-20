b166fe95e77e49fb884d1671f8296cfce3ffd16a
Caturano Federico <federico.caturano@euris.it>
2026-02-20 10:16:13 +0100

    Created library project in .NET solution

---
+++ Core/Services/CalculatorService.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+
+        public async Task<int> MultiplyAsync(int a, int b)
+        {
+            await Task.Delay(50);
+            return a * b;
+        }
+    }
+}
