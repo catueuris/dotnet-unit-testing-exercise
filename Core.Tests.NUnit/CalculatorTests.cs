155ed2f57a1a3531d1bb6f78a3ce0febc7dc848b
Caturano Federico <federico.caturano@euris.it>
2026-02-20 10:40:34 +0100

    Added NUnit test project to solution

---
+++ Core.Tests.NUnit/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Tests.NUnit
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
